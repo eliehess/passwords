3ba4f8f83c5e14d8a5900cba1b2e4c64a3edb005
Elie <eliemhess@gmail.com>
2021-09-03 13:10:43 -0700

    Refactor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,62 @@
 use std::io::{self, Write};
-use passwords::hash::hash;
+use std::result;
+use passwords::hash;
 use rpassword;
 use sqlite;
 
-fn main() {
-    print!("Enter password: ");
+fn main() -> result::Result<(), String> {
+    print_and_flush("Enter password: ")?;
 
-    match io::stdout().flush() {
-        Ok(()) => (),
-        Err(e) => { eprintln!("Error flushing stdout: {}", e); return; }
-    };
+    let password = read_password()?;
 
-    let password = match rpassword::read_password() {
-        Ok(input) => input,
-        Err(e) => { eprintln!("Error reading input: {}", e); return; }
-    };
-
-    let hashed_password = hash(password);
+    let hashed_password = hash::hash(password);
 
     if hashed_password.as_str() != "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" {
-        println!("Incorrect password");
-        return;
+        return Err(String::from("Incorrect password"));
     }
 
     println!("Correct password");
 
-    let connection = match sqlite::open("passwords.db") {
-        Ok(file) => file,
-        Err(e) => { eprintln!("Error connecting to database: {}", e); return; }
-    };
+    let connection = get_db_connection("passwords.db")?;
 
-    match connection.execute("CREATE TABLE IF NOT EXISTS passwords (name TEXT, password TEXT, PRIMARY KEY (name))") {
-        Ok(()) => (),
-        Err(e) => { eprintln!("Error creating table: {}", e); return; }
-    };
+    init_db(&connection)?;
 
-    print!("Enter option: ");
-    match io::stdout().flush() {
-        Ok(()) => (),
-        Err(e) => { eprintln!("Error flushing stdout: {}", e); return; }
-    };
+    print_and_flush("Enter option: ")?;
 
-    let option = match read_input() {
-        Ok(input) => input,
-        Err(e) => { eprintln!("Error reading input: {}", e); return; }
-    };
+    let option = read_input()?;
 
     match option.as_str() {
         "add" => {
-            println!("Enter name of password to add:");
-            let name = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            print_and_flush("Enter name of password to add: ")?;
+            let name = read_input()?;
 
-            println!("Enter password to add:");
-            let password = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            print_and_flush("Enter password to add: ")?;
+            let password = read_password()?;
             
-            println!("Confirm password to add:");
-            let password_confirm = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            print_and_flush("Confirm password to add: ")?;
+            let password_confirm = read_password()?;
 
             if password != password_confirm {
-                println!("Passwords don't match. Aborting.");
-                return;
+                return Err(String::from("Passwords don't match. Aborting."));
             }
 
             match add_password(&connection, &name, &password) {
                 Ok(()) => { println!("Added password for {}!", name) },
-                Err(e) => { eprintln!("Error adding database entry: {}", e); return; }
+                Err(e) => { return Err(format!("Error adding database entry: {}", e)); }
             };
         },
         "get" => {
-            println!("Enter name of password to get:");
+            println!("Enter name of password to get: ");
 
-            let name = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            let name = read_input()?;
 
             let results = match get_password(&connection, &name) {
                 Ok(res) => res,
-                Err(e) => { eprintln!("Error getting database entry: {}", e); return; }
+                Err(e) => { return Err(format!("Error getting database entry: {}", e)); }
             };
 
             if results.len() == 0 {
-                println!("No results found for name {}", name);
-                return;
+                return Err(format!("No results found for name {}", name));
             }
 
             for result in results {
@@ -99,22 +64,17 @@ fn main() {
             }
         },
         "all" => { 
-            println!("Are you sure you want to get all passwords? They will be printed to the console. y/N: ");
-            let confirm = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
-
+            print_and_flush("Are you sure you want to get all passwords? They will be printed to the console. y/N: ")?;
+            let confirm = read_input()?;
             match confirm.as_str() {
                 "y" | "Y" => { 
                     let results = match get_all_passwords(&connection) {
                         Ok(res) => res,
-                        Err(e) => { eprintln!("Error getting all passwords: {}", e); return; }
+                        Err(e) => { return Err(format!("Error getting all passwords: {}", e)); }
                     };
 
                     if results.len() == 0 {
-                        println!("No passwords found");
-                        return;
+                        return Err(format!("No passwords found"));
                     }
         
                     for result in results {
@@ -126,29 +86,25 @@ fn main() {
         },
         "remove" => {
             println!("Enter name of password to remove:");
-            let name = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            let name = read_input()?;
 
             println!("Are you sure you want to remove? y/N: ");
-            let confirm = match read_input() {
-                Ok(input) => input,
-                Err(e) => { eprintln!("Error reading input: {}", e); return; }
-            };
+            let confirm = read_input()?;
 
             match confirm.as_str() {
                 "y" | "Y" => { 
                     match remove_password(&connection, &name) {
                         Ok(()) => { println!("Successfully removed password for {}", name); },
-                        Err(e) => { eprintln!("Error removing password: {}", e); return; }
+                        Err(e) => { return Err(format!("Error removing password: {}", e)); }
                     };
                 },
                 _ => { println!("Cancelling removal"); }
             };
         },
-        _ => { println!("Invalid option. Supported options: add, get, all, remove"); return; }
-    }
+        _ => { return Err(format!("Invalid option. Supported options: add, get, all, remove")); }
+    };
+
+    Ok(())
 }
 
 fn add_password(connection: &sqlite::Connection, name: &str, password: &str) -> sqlite::Result<()> {
@@ -189,8 +145,40 @@ fn get_all_passwords(connection: &sqlite::Connection) -> sqlite::Result<Vec<Stri
     Ok(fin)
 }
 
-fn read_input() -> io::Result<String> {
+fn read_input() -> result::Result<String, String> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    Ok(input.trim().to_string())
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => Ok(input.trim().to_string()),
+        Err(e) => Err(format!("Error reading input: {}", e))
+    }
+}
+
+fn print_and_flush(output: &str) -> result::Result<(), String> {
+    print!("{}", output);
+
+    match io::stdout().flush() {
+        Ok(()) => Ok(()),
+        Err(e) => Err(format!("Error flushing stdout: {}", e))
+    }
+}
+
+fn get_db_connection(location: &str) -> result::Result<sqlite::Connection, String> {
+    match sqlite::open(location) {
+        Ok(file) => Ok(file),
+        Err(e) => Err(format!("Error connecting to database: {}", e))
+    }
+}
+
+fn init_db(connection: &sqlite::Connection) -> result::Result<(), String> {
+    match connection.execute("CREATE TABLE IF NOT EXISTS passwords (name TEXT, password TEXT, PRIMARY KEY (name))") {
+        Ok(()) => Ok(()),
+        Err(e) => Err(format!("Error initializing database: {}", e))
+    }
+}
+
+fn read_password() -> result::Result<String, String> {
+    match rpassword::read_password() {
+        Ok(result) => Ok(result),
+        Err(e) => Err(format!("Error reading input: {}", e))
+    }
 }
