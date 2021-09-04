ca0b0761a1d2deec488b9e1dfb96105da3154201
Elie <eliemhess@gmail.com>
2021-09-04 15:18:21 -0700

    Update error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod db {
     use sqlite;
-    use std::{result, path};
+    use std::{path, error::Error};
     use super::encryption;
 
     static DB_LOCATION: &str = "passwords.db";
@@ -11,7 +11,7 @@ pub mod db {
     }
 
     impl Database {
-        pub fn new(path: &path::PathBuf, encryption: encryption::Encryption) -> result::Result<Database, String> {
+        pub fn new(path: &path::PathBuf, encryption: encryption::Encryption) -> Result<Database, String> {
             let connection = match sqlite::open(path.join(DB_LOCATION)) {
                 Ok(c) => c,
                 Err(e) => return Err(format!("Unable to connect to database: {}", e))
@@ -25,12 +25,12 @@ pub mod db {
             }
         }
 
-        pub fn add_password(&self, name: &str, password: &str) -> sqlite::Result<()> {
-            let enc_password = hex::encode(self.encryption.encrypt(password));
-            self.connection.execute(format!("INSERT INTO passwords VALUES ('{}', '{}')", name, enc_password))
+        pub fn add_password(&self, name: &str, password: &str) -> Result<(), Box<dyn Error>> {
+            let enc_password = hex::encode(self.encryption.encrypt(password)?);
+            Ok(self.connection.execute(format!("INSERT INTO passwords VALUES ('{}', '{}')", name, enc_password))?)
         }
         
-        pub fn get_password(&self, name: &str, password: &str) -> sqlite::Result<Vec<String>> {
+        pub fn get_password(&self, name: &str, password: &str) -> Result<Vec<String>, Box<dyn Error>> {
             let mut statement = self.connection.prepare("SELECT password FROM passwords WHERE name = ?")?;
         
             statement.bind(1, name)?;
@@ -38,7 +38,7 @@ pub mod db {
             let mut fin: Vec<String> = Vec::new();
         
             while let sqlite::State::Row = statement.next()? {
-                fin.push(self.encryption.decrypt(&hex::decode(statement.read::<String>(0)?).unwrap(), &password));
+                fin.push(self.encryption.decrypt(&hex::decode(statement.read::<String>(0)?)?, &password)?);
             }
         
             Ok(fin)
@@ -48,14 +48,14 @@ pub mod db {
             self.connection.execute(format!("DELETE FROM passwords WHERE name = '{}'", name))
         }
         
-        pub fn get_all_passwords(&self, password: &str) -> sqlite::Result<Vec<String>> {
+        pub fn get_all_passwords(&self, password: &str) ->Result<Vec<String>, Box<dyn Error>> {
             let mut statement = self.connection.prepare("SELECT name, password FROM passwords ORDER BY name ASC")?;
         
             let mut fin: Vec<String> = Vec::new();
         
             while let sqlite::State::Row = statement.next()? {
                 let name = statement.read::<String>(0)?;
-                let password = self.encryption.decrypt(&hex::decode(statement.read::<String>(1)?).unwrap(), &password);
+                let password = self.encryption.decrypt(&hex::decode(statement.read::<String>(1)?)?, &password)?;
                 fin.push(format!("{}: {}", name, password));
             }
         
@@ -67,7 +67,8 @@ pub mod db {
 pub mod encryption {
     use openssl::rsa::{Padding, Rsa};
     use openssl::symm::Cipher;
-    use std::{fs, path, result};
+    use std::{fs, path, result, error::Error};
+    use std::io;
     use super::utils;
     
     static PUBLIC_KEY: &str = "public.key";
@@ -81,33 +82,33 @@ pub mod encryption {
     }
 
     impl Encryption {
-        pub fn use_existing(path: &path::PathBuf) -> Result<Encryption, String> {
+        pub fn use_existing(path: &path::PathBuf) -> Result<Encryption, Box<dyn Error>> {
             if do_keys_exist(&path) {
-                let public_key = utils::read_file(&path.join(PUBLIC_KEY)).unwrap();
-                let private_key = utils::read_file(&path.join(PRIVATE_KEY)).unwrap();
-                let password_hash = String::from_utf8(utils::read_file(&path.join(PASSWORD_HASH)).unwrap()).unwrap();
+                let public_key = utils::read_file(&path.join(PUBLIC_KEY))?;
+                let private_key = utils::read_file(&path.join(PRIVATE_KEY))?;
+                let password_hash = String::from_utf8(utils::read_file(&path.join(PASSWORD_HASH))?)?;
 
                 Ok(Encryption { public_key, private_key, password_hash })
             } else {
-                Err(String::from("Keys don't exist"))
+                Err(Box::new(io::Error::new(io::ErrorKind::Other, "Keys don't exist")))
             }
         }
 
-        pub fn make_new(path: &path::PathBuf, password: &str) -> Encryption {
-            let keypair = Rsa::generate(2048).unwrap();
+        pub fn make_new(path: &path::PathBuf, password: &str) -> Result<Encryption, Box<dyn Error>> {
+            let keypair = Rsa::generate(2048)?;
             let cipher = Cipher::aes_256_cbc();
-            let public_key = keypair.public_key_to_pem_pkcs1().unwrap();
-            let private_key = keypair.private_key_to_pem_passphrase(cipher, password.as_bytes()).unwrap();
+            let public_key = keypair.public_key_to_pem_pkcs1()?;
+            let private_key = keypair.private_key_to_pem_passphrase(cipher, password.as_bytes())?;
             let password_hash = utils::hash(&password);
             
             if !path.is_dir() {
-                fs::create_dir_all(&path).unwrap();
+                fs::create_dir_all(&path)?;
             }
     
-            fs::write(path.join(PUBLIC_KEY), &public_key).unwrap();
-            fs::write(path.join(PRIVATE_KEY), &private_key).unwrap();
-            fs::write(path.join(PASSWORD_HASH), &password_hash).unwrap();
-            Encryption { public_key, private_key, password_hash }
+            fs::write(path.join(PUBLIC_KEY), &public_key)?;
+            fs::write(path.join(PRIVATE_KEY), &private_key)?;
+            fs::write(path.join(PASSWORD_HASH), &password_hash)?;
+            Ok(Encryption { public_key, private_key, password_hash })
         }
 
         pub fn check_password(&self, password: &str) -> result::Result<(), String> {
@@ -121,18 +122,18 @@ pub mod encryption {
             }
         }
 
-        pub fn encrypt(&self, text: &str) -> Vec<u8> {
-            let pubkey = Rsa::public_key_from_pem_pkcs1(&self.public_key).unwrap();
+        pub fn encrypt(&self, text: &str) -> Result<Vec<u8>, openssl::error::ErrorStack> {
+            let pubkey = Rsa::public_key_from_pem_pkcs1(&self.public_key)?;
             let mut encrypted = vec![0; pubkey.size() as usize];
-            pubkey.public_encrypt(text.as_bytes(), &mut encrypted, Padding::PKCS1).unwrap();
-            encrypted
+            pubkey.public_encrypt(text.as_bytes(), &mut encrypted, Padding::PKCS1)?;
+            Ok(encrypted)
         }
     
-        pub fn decrypt(&self, text: &Vec<u8>, password: &str) -> String {
-            let privkey = Rsa::private_key_from_pem_passphrase(&self.private_key, password.as_bytes()).unwrap(); 
+        pub fn decrypt(&self, text: &Vec<u8>, password: &str) -> Result<String, Box<dyn Error>> {
+            let privkey = Rsa::private_key_from_pem_passphrase(&self.private_key, password.as_bytes())?; 
             let mut decrypted = vec![0; privkey.size() as usize];
-            let len = privkey.private_decrypt(&text, &mut decrypted, Padding::PKCS1).unwrap();
-            return String::from_utf8(decrypted[..len].to_vec()).unwrap();
+            let len = privkey.private_decrypt(&text, &mut decrypted, Padding::PKCS1)?;
+            Ok(String::from_utf8(decrypted[..len].to_vec())?)
         }
     }
 
@@ -156,8 +157,8 @@ pub mod utils {
         Ok(data)
     }
 
-    pub fn print_and_flush(output: &str) {
-        print!("{}", output);
+    pub fn print_and_flush(output: impl AsRef<str>) {
+        print!("{}", output.as_ref());
         io::stdout().flush().expect("Unable to flush stdout");
     }
     
@@ -169,11 +170,8 @@ pub mod utils {
         }
     }
     
-    pub fn read_password() -> result::Result<String, String> {
-        match rpassword::read_password() {
-            Ok(result) => Ok(result),
-            Err(e) => Err(format!("Error reading password: {}", e))
-        }
+    pub fn read_password() -> io::Result<String> {
+        rpassword::read_password()
     }
     
     pub fn hash(input: impl AsRef<[u8]>) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use std::{result, env};
+use std::{result::Result, env, error::Error};
 use passwords::{db, encryption, utils::*};
 
-fn main() -> result::Result<(), String> {
-    let data_dir = env::current_exe().unwrap().parent().unwrap().join(".data");
+fn main() -> Result<(), Box<dyn Error>> {
+    let data_dir = env::current_exe()?.parent().unwrap().join(".data");
 
     let encryption = match encryption::Encryption::use_existing(&data_dir) {
-        Ok(x) => x,
+        Ok(enc) => enc,
         Err(_e) => {
             println!("Welcome! It looks like you haven't set up this application yet.");
 
-            print_and_flush("Before continuing, please choose a master password: ");
-            let init_password = read_password()?;
+            let password = loop {
+                print_and_flush("Please choose a master password: ");
+                let init_password = read_password()?;
+    
+                print_and_flush("Please confirm your master password: ");
+                let confirm_init_password = read_password()?;
+    
+                if init_password == confirm_init_password {
+                    break init_password;
+                }
 
-            print_and_flush("Please confirm your master password: ");
-            let confirm_init_password = read_password()?;
+                println!("Your passwords don't match. Please try again.");
+            };
 
-            if init_password == confirm_init_password {
-                println!("Awesome! You're ready to go.");
-                encryption::Encryption::make_new(&data_dir, &init_password)
-            } else {
-                return Err(String::from("Whoops! Your passwords don't match"));
+            match encryption::Encryption::make_new(&data_dir, &password) {
+                Ok(enc) => {
+                    println!("Awesome! You're ready to go.");
+                    enc
+                },
+                Err(e) => {
+                    println!("Something went wrong when initializing application: {}", e);
+                    return Ok(())
+                }
             }
         }
     };
@@ -29,89 +41,110 @@ fn main() -> result::Result<(), String> {
 
     encryption.check_password(&password)?;
 
-    print_and_flush("Enter option (add, get, all, remove): ");
-    let option = read_input()?;
-
     let db = db::Database::new(&data_dir, encryption)?;
 
-    match option.as_str() {
-        "add" => {
-            print_and_flush("Enter name of password to add: ");
-            let name_to_add = read_input()?;
+    loop {
+        print_and_flush("Enter option (add, get, all, remove): ");
+        let option = read_input()?;
 
-            print_and_flush("Enter password to add: ");
-            let password_to_add = read_password()?;
-            
-            print_and_flush("Confirm password to add: ");
-            let password_confirm = read_password()?;
+        match option.as_str() {
+            "add" => {
+                print_and_flush("Enter name of password to add: ");
+                let name_to_add = read_input()?;
 
-            if password_to_add != password_confirm {
-                return Err(String::from("Passwords don't match"));
-            }
+                let password_to_add = loop {
+                    print_and_flush(format!("Enter password to add for {}: ", name_to_add));
+                    let password_to_add = read_password()?;
+                    
+                    print_and_flush(format!("Confirm password to add for {}: ", name_to_add));
+                    let password_confirm = read_password()?;
 
-            match db.add_password(&name_to_add, &password_to_add) {
-                Ok(()) => println!("Added password for {}!", name_to_add),
-                Err(e) => return Err(format!("Error adding database entry: {}", e))
-            };
-        },
-        "get" => {
-            print_and_flush("Enter name of password to get: ");
-
-            let name_to_get = read_input()?;
-
-            let results = match db.get_password(&name_to_get, &password) {
-                Ok(res) => res,
-                Err(e) => return Err(format!("Error getting database entry: {}", e))
-            };
-
-            if results.len() == 0 {
-                return Err(format!("No results found for name {}", name_to_get));
-            }
-
-            for result in results {
-                println!("password for {}: {}", name_to_get, result);
-            }
-        },
-        "all" => { 
-            print_and_flush("Are you sure you want to get all passwords? They will be printed to the console. y/N: ");
-            let confirm = read_input()?;
-            match confirm.as_str() {
-                "y" | "Y" => { 
-                    let results = match db.get_all_passwords(&password) {
-                        Ok(res) => res,
-                        Err(e) => return Err(format!("Error getting all passwords: {}", e))
-                    };
-
-                    if results.len() == 0 {
-                        return Err(format!("No passwords found"));
+                    if password_to_add == password_confirm {
+                        break password_to_add;
                     }
-        
-                    for result in results {
-                        println!("{}", result);
+
+                    println!("The passwords don't match. Please try again.");
+                };
+
+                match db.add_password(&name_to_add, &password_to_add) {
+                    Ok(()) => println!("Added password for {}!", name_to_add),
+                    Err(e) => println!("Unable to add password for {}: {}", name_to_add, e)
+                };
+
+                break;
+            },
+            "get" => {
+                print_and_flush("Enter name of password to get: ");
+
+                let name_to_get = read_input()?;
+
+                let results = match db.get_password(&name_to_get, &password) {
+                    Ok(res) => res,
+                    Err(e) => {
+                        println!("Unable to get password for {}: {}", name_to_get, e);
+                        break;
                     }
-                },
-                _ => println!("Cancelling retrieving all passwords")
-            };
-        },
-        "remove" => {
-            println!("Enter name of password to remove:");
-            let name_to_remove = read_input()?;
+                };
 
-            println!("Are you sure you want to remove password for {}? y/N: ", name_to_remove);
-            let confirm = read_input()?;
+                match results.len() {
+                    0 => println!("No passwords found for {}", name_to_get),
+                    1 => println!("{}", results.get(0).unwrap()),
+                    _ => println!("Somehow there's more than one entry for {}", name_to_get)
+                }
 
-            match confirm.as_str() {
-                "y" | "Y" => {
-                    match db.remove_password(&name_to_remove) {
-                        Ok(()) => println!("Successfully removed password for {}", name_to_remove),
-                        Err(e) => return Err(format!("Error removing password: {}", e))
-                    };
-                },
-                _ => println!("Cancelling removal")
-            };
-        },
-        _ => return Err(format!("Invalid option. Supported options: add, get, all, remove"))
-    };
+                break;
+            },
+            "all" => { 
+                print_and_flush("Are you sure you want to get all passwords? They will be printed to the console. y/N: ");
+                let confirm = read_input()?;
+                match confirm.as_str() {
+                    "y" | "Y" => { 
+                        let results = match db.get_all_passwords(&password) {
+                            Ok(res) => res,
+                            Err(e) => {
+                                println!("Unable to get all passwords: {}", e);
+                                break;
+                            }
+                        };
+
+                        if results.len() == 0 {
+                            println!("No passwords found");
+                        } else {
+                            for result in results {
+                                println!("{}", result);
+                            }
+                        }
+                    },
+                    _ => println!("Cancelling retrieving all passwords")
+                };
+                
+                break;
+            },
+            "remove" => {
+                println!("Enter name of password to remove:");
+                let name_to_remove = read_input()?;
+
+                println!("Are you sure you want to remove password for {}? y/N: ", name_to_remove);
+                let confirm = read_input()?;
+
+                match confirm.as_str() {
+                    "y" | "Y" => {
+                        match db.remove_password(&name_to_remove) {
+                            Ok(()) => println!("Successfully removed password for {}", name_to_remove),
+                            Err(e) => {
+                                println!("Unable to remove password for {}: {}", name_to_remove, e);
+                                return Ok(());
+                            }
+                        };
+                    },
+                    _ => println!("Cancelling removal")
+                };
+
+                break;
+            },
+            _ => println!("Invalid option. Supported options: add, get, all, remove")
+        };
+    }
 
     Ok(())
 }
